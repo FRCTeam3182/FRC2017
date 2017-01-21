91bc84755cdf449243d6af874f3119940e1eb585
Colman Glagovich <cglagovich@gmail.com>
2017-01-21 13:33:10 -0500

    Comments on DriveControl
    I added some comments to the DriveControl class.

--- src/org/usfirst/frc/team3182/robot/DriveControl.java
+++ src/org/usfirst/frc/team3182/robot/DriveControl.java
@@ -24,38 +24,55 @@ public class DriveControl {
 	public DriveControl() {
 		
 		System.out.println("Drive Control Initialized");
-		
+		//this tells Robot that the robot is using power glove
 		Robot.usesPowerGlove = true; //Implement this in Robot class
 		System.out.println("PowerGlove used");
 		
 		
 	}
 	
+	/**
+	 * The standard get left power method, linear with .15 deadzone
+	 */
 	public double getL() {
 		if(Math.abs(driveControlL.getY())<.15) return 0;
 		return driveControlL.getY();
 		
 	}
+	/**
+	 * The standard get right power method, linear with .15 deadzone
+	 */
 	public double getR() {
 		if(Math.abs(driveControlR.getY())<.15) return 0;
 		return driveControlR.getY();
 	
 		
 	}
-	
+	/**
+	 * This method returns the position of the left joystick with a square rule
+	 * 
+	 * @return square rule of getY() for left joystick
+	 */
 	public double getLExp() { //"ramps up"
     	if(Math.abs(getL())<.1)return 0; // Deadzone
     	if (getL() > 0) return Math.pow(getL(), 2);
         else return -Math.abs(Math.pow(getL(), 2));
     }
-	
+	/**
+	 * This method returns the position of the right joystick with a square rule
+	 * 
+	 * @return square rule of getY() for right joystick
+	 */
 	public double getRExp() {
     	if(Math.abs(getR())<.1) return 0; // Deadzone
         if (getR() > 0) return Math.pow(getR(), 2);
         else return -Math.abs(Math.pow(getR(), 2));
     }
 	
-	
+	/**
+	 * The roll of the powerglove is the motion that you would make to steer a car
+	 * @return the Y value of the power glove
+	 */
 	public double getPowerGloveRoll() {
 		return powerGlove.getY();
 		
