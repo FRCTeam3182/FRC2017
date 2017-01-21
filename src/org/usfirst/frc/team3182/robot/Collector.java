65e28067b633b5377275652b52239f3eb776c719
Alex Torino <torinoalexp@gmail.com>
2017-01-21 13:33:22 -0500

    Finished methods, but ultrasonic and encoder values are still needed.

--- src/org/usfirst/frc/team3182/robot/Collector.java
+++ src/org/usfirst/frc/team3182/robot/Collector.java
@@ -17,7 +17,6 @@ public class Collector {
 	private Encoder encoder;
 	
 	public Collector() {
-		
 		upperMotor = new Talon(2);
 		lowerMotor = new Talon(3);
 		ultrasonic = new Ultrasonic(0, 0);
@@ -25,14 +24,21 @@ public class Collector {
 	}
 	
 	public void collect() {
-		
-		
+		upperMotor.set(0.5);
+		lowerMotor.set(0.5);
+		upperMotor.getSpeed();
+		lowerMotor.getSpeed();
 	}
 	public boolean detectGear() {
-		
+		//FIXME find ultrasonic value for gear
+		ultrasonic.ping();
+		ultrasonic.getRangeInches();
+		return false;
 	}
 	public boolean detectMotorJammed() {
-		
+		//FIXME find encoder value for jammed motor
+		encoder.getStopped();
+		return false;
 	}
 	
 }
