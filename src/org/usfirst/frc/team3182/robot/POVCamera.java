55ba2659d291ba533877902695ab077cdb05dd80
Alex Torino <torinoalexp@gmail.com>
2017-03-09 20:53:57 -0500

    Dpad Tilt Fiz
    I'm almost done with the dpad

--- src/org/usfirst/frc/team3182/robot/POVCamera.java
+++ src/org/usfirst/frc/team3182/robot/POVCamera.java
@@ -9,116 +9,120 @@ import edu.wpi.first.wpilibj.Servo;
  */
 
 
-
 public class POVCamera {
-	
+
 	//Pan servo of camera mount
-		Servo pan = RobotConfig.pan;
-		
-		//Tilt servo of camera mount
-		Servo tilt = RobotConfig.pan;
-	
-		
-		
-		/* 
-		 * Using driveControlR for dpad
-		 * driveControlR is the joystick with the dpad(knob on top)
-		 */
-Joystick stick = new Joystick(RobotConfig.joystickRChannel);
+	Servo pan = RobotConfig.pan;
+
+	//Tilt servo of camera mount
+	Servo tilt = RobotConfig.tilt;
 
 
-//Control
-public void dpadMove() {
-	
-	if (stick.getPOV() == 0){
 
-		tilt.set(tilt.get() + 0.02);
 
-		}
-	//up
-	
-	if (stick.getPOV() == 45){
 
-		tilt.set(tilt.get() + 0.02);
-		pan.set(pan.get() + 0.02);
-
-		}
-	//up right
-	
-	if (stick.getPOV() == 90){
-
-			pan.set(pan.get() + 0.02);
-
-		}
-	//right
-	
-	if (stick.getPOV() == 135){
-
-				tilt.set(tilt.get() - 0.02);
-				pan.set(pan.get() + 0.02);
-
-		}
-	//down right
-	
-	if (stick.getPOV() == 180){
-
-		tilt.set(tilt.get() - 0.02);
-
-		}
-	//down
-	
-	if (stick.getPOV() == 225){
-
-		tilt.set(tilt.get() - 0.02);
-		pan.set(pan.get() - 0.02);
-
-		}
-	//down left
-	
-	if (stick.getPOV() == 270){
-
-		pan.set(pan.get() - 0.02);
-
-		}
-	//left
-	
-	if (stick.getPOV() == 315){
-
-		tilt.set(tilt.get() + 0.02);
-		pan.set(pan.get() - 0.02);
-
-		}
-	//up left
-	
-	if (stick.getPOV() == -1) {
-		tilt.set(0.5);
-		pan.set(0.5);
-	//when let go recenter camera
-		
-	if (tilt.get() < 0) {
-		tilt.set(0);
-		//limit tilt value lower than 0
-		
-	if (tilt.get() > 1) {
-		tilt.set(1);
-	}
-	//limit tilt value above 1 
-	
-	if (pan.get() < 0) {
-		pan.set(0);
-	}
-	//limit pan value lower than 0
-	
-	if (pan.get() > 1) {
-		pan.set(1);
-	}
-	}
-	}
-	}
-
+	/* 
+	 * Using driveControlR for dpad
+	 * driveControlR is the joystick with the dpad(knob on top)
+	 */
+	Joystick stick = new Joystick(RobotConfig.joystickRChannel);
 
 	public POVCamera() {
-		// TODO 
+		tilt.set(0.5);
+		pan.set(0.5);
 	}
+
+	//Control
+	public void dpadMove() {
+
+		if (stick.getPOV() == 0){
+
+			tilt.set(tilt.get() + 0.01);
+
+		}
+		//up
+
+		if (stick.getPOV() == 45){
+
+			tilt.set(tilt.get() + 0.01);
+			pan.set(pan.get() + 0.01);
+
+		}
+		//up right
+
+		if (stick.getPOV() == 90){
+
+			pan.set(pan.get() + 0.01);
+
+		}
+		//right
+
+		if (stick.getPOV() == 135){
+
+			tilt.set(tilt.get() - 0.01);
+			pan.set(pan.get() + 0.01);
+
+		}
+		//down right
+
+		if (stick.getPOV() == 180){
+
+			tilt.set(tilt.get() - 0.01);
+
+		}
+		//down
+
+		if (stick.getPOV() == 225){
+
+			tilt.set(tilt.get() - 0.01);
+			pan.set(pan.get() - 0.01);
+
+		}
+		//down left
+
+		if (stick.getPOV() == 270){
+
+			pan.set(pan.get() - 0.01);
+
+		}
+		//left
+
+		if (stick.getPOV() == 315){
+
+			tilt.set(tilt.get() + 0.01);
+			pan.set(pan.get() - 0.01);
+
+		}
+		//up left
+
+		if (stick.getPOV() == -1) {
+			tilt.set(0.5);
+			pan.set(0.5);
+		}
+		
+		//when let go recenter camera
+
+		if (tilt.get() < 0) {
+			tilt.set(0.5);
+		}
+		//limit tilt value lower than 0
+
+		if (tilt.get() > 1) {
+			tilt.set(0.5);
+		}
+		//limit tilt value above 1 
+
+		if (pan.get() < 0) {
+			pan.set(0.5);
+		}
+		//limit pan value lower than 0
+
+		if (pan.get() > 1) {
+			pan.set(0.5);
+		}
+
+	}
+
+
 
 }
